cc90e56a1a72c87862b0e5d3fe98016058a56e9b
whilealive <bruno.bischi@gmail.com>
2018-06-12 16:11:36 +0200

    input all tex-files in some folder-structure

---
+++ collectalltexfiles.lua
@@ -0,0 +1,65 @@
+-- ==================================================================
+-- FILE     collectalltexfiles.lua
+-- INFO     recurses down a directory tree and inputs all the tex-files found
+--          https://tex.stackexchange.com/questions/7653/how-to-iterate-through-the-name-of-files-in-a-folder
+--
+--          use as follows (luacode-package required!):
+--          \begin{luacode}
+--            require("collectalltexfiles.lua")
+--            CollectAllTexFiles(lfs.currentdir() .. "/DL-GESO-Aufgaben/")
+--          \end{luacode}
+--
+-- DATE     12.06.2018
+-- OWNER    Bischofberger
+-- ==================================================================
+--
+-- FIXME: filenames are not sorted...
+
+
+function dirtree(dir)
+  if string.sub(dir, -1) == "/" then
+    dir=string.sub(dir, 1, -2)
+  end
+
+  local function yieldtree(dir)
+    for entry in lfs.dir(dir) do
+      if not entry:match("^%.") then
+        entry=dir.."/"..entry
+          if lfs.isdir(entry) then
+            yieldtree(entry)
+          else
+            coroutine.yield(entry)
+          end
+      end
+    end
+  end
+
+  return coroutine.wrap(function() yieldtree(dir) end)
+end
+
+
+function GetFileExtension(filename)
+  return filename:match("^.+(%..+)$")
+end
+
+
+function isValidTexFile(filename)
+  if filename:match("_QuickCompile") then
+    return false
+  end
+  if GetFileExtension(filename) ~= ".tex" then
+    return false
+  end
+  return true
+end
+
+
+function CollectAllTexFiles(dir)
+  for i in dirtree(dir) do
+    local filename = i:gsub(".*/([^/]+)$","%1")
+    if isValidTexFile(filename) then
+      --tex.sprint(filename .. "\\BZ")
+      tex.sprint("[" .. filename .. "]\\BZ" .. "\\input " .. dir ..  filename .. " " .. "\\clearpage\\par")
+    end
+  end
+end
